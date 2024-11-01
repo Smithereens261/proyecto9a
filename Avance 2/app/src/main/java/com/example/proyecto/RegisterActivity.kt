0639387c41af66ed46c4b5484a26f9e68aff51c6
Olguin Soto Edgar Rafael <132985798+Smithereens261@users.noreply.github.com>
2024-10-31 22:53:50 -0700

    Add files via upload

---
+++ Avance 2/app/src/main/java/com/example/proyecto/RegisterActivity.kt
@@ -0,0 +1,34 @@
+package com.example.proyecto
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.proyecto.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginTV.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
+        binding.createAccountBtn.setOnClickListener{
+            val email = binding.emailRegister.text.toString()
+            val password = binding.passwordRegister.text.toString()
+            if(email.isNotEmpty() && password.isNotEmpty())
+                MainActivity.auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        startActivity(Intent(this, LoginActivity::class.java))
+                        finish()
+                    }
+                }.addOnFailureListener{
+                    Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+        }
+    }
+}
